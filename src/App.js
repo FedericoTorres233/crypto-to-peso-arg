d9a022fed473ad41b529db42ca76d03e3acb04fb
FedericoTorres233 <federicofederico881@gmail.com>
2022-01-24 16:48:36 -0300

    add grid layout - remove fragment

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import { Fragment } from "react";
 import { Routes, Route } from "react-router-dom";
 
 import NavBar from "./components/NavBar";
@@ -6,16 +5,19 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Footer from "./components/Footer";
 
+import classes from "./App.module.css";
+
 function App() {
   return (
-    <Fragment>
+    <div className={classes.grid}>
       <NavBar />
       <Routes>
+        {/*Defines the routes*/}
         <Route exact path="/" element={<Home />} />
         <Route exact path="/about" element={<About />} />
       </Routes>
       <Footer />
-    </Fragment>
+    </div>
   );
 }
 
