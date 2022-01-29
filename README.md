529b48340eced9374e56cbc81ab700c2b6eb39e9
FedericoTorres233 <federicofederico881@gmail.com>
2022-01-29 17:05:09 -0300

    added more styles🎨! and finished about page

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
-# Crypto to Argentine peso
+# Crypto to Argentine peso 🪙
 
-## A web created to convert Argentine pesos into cryptocurrencies
+A web created to convert Argentine pesos into cryptocurrencies.
+
+---
+
+## **Visit my page** [here!](https://arg-to-crypto.herokuapp.com "Let's go!")
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,17 +1,31 @@
+import classes from "./About.module.css";
+
 function About() {
   return (
-    <div>
-      <h1>Hello! My Name is Federico</h1>
-      <h3>
+    <div className={classes.all}>
+      <h1 className={classes.h1}>Hello! My Name is Federico</h1>
+      <h3 className={classes.h3}>
         This webapp is something I did in my free time to practice how to code.
         If you have any questions, don't hesitate to{" "}
-        <a href="mailto:federicofederico881@gmail.com">email me📩</a>
-        <h4>
-          <a href="https://github.com/FedericoTorres233">Source Code</a>
-        </h4>
-        <a href="https://github.com/FedericoTorres233">
-          <img src="%PUBLIC_URL%/assets/githubLogo.png" alt="github"></img>
+        <a className={classes.a} href="/">
+          email me 📩
         </a>
+        <h4 className={classes.h4}>
+          <a className={classes.a} href="https://github.com/FedericoTorres233/crypto-to-peso-arg">
+            Source Code
+          </a>
+        </h4>
+        <h4 className={classes.h4}>
+          <a className={classes.a} href="https://github.com/FedericoTorres233">
+            Follow me on{" "}
+            <img
+              className={classes.a.img}
+              src="https://cdn-icons-png.flaticon.com/512/25/25231.png"
+              alt="github"
+            ></img>
+            {" :)"}
+          </a>
+        </h4>
       </h3>
     </div>
   );
