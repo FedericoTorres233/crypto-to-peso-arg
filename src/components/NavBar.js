d35a9e94966058d31e17aa9086a2b9e055e7500f
FedericoTorres233 <federicofederico881@gmail.com>
2022-01-29 00:47:26 -0300

    add styling to navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,16 +1,16 @@
 import { Link } from "react-router-dom";
 
+import classes from "./NavBar.module.css"
+
 function NavBar() {
   return (
     <header>
       <h1>Crypto to Argentine Peso</h1>
       <h2>A simple web to convert cryptocurrencies into Argentine Pesos</h2>
-      <ul>
+      <ul className={classes.all}>
         <li>
           <Link to="/">Home</Link>
         </li>
-      </ul>
-      <ul>
         <li>
           <Link to="/about">About</Link>
         </li>
